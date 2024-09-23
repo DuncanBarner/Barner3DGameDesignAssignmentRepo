9ac814f10a6e1f59e1035c1f738811c1fad8bb7f
Duncan Barner <duncan.barner@gmail.com>
2024-09-23 10:57:02 -0500

    Finished First Half of Prototype 3 Video

---
+++ 3DPrototype1DuncanBarner/Assets/Scripts/P3/Pro3MoveLeft.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pro3MoveLeft : MonoBehaviour
+{
+
+    public float speed;
+    private float leftBound = -15f;
+    private Pro3PlayerController playerControllerScript;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Pro3PlayerController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ 3DPrototype1DuncanBarner/Assets/Scripts/P3/Pro3SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pro3SpawnManager : MonoBehaviour
+{
+
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float startDelay = 2f;
+    private float repeatRate = 2f;
+    private Pro3PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Pro3PlayerController>();
+    }
+
+    void SpawnObstacle()
+    {
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
