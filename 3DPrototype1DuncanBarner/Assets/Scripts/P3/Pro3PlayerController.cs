0a692dd8e0e514739879220814dfda7c6d3fd6f2
Duncan Barner <duncan.barner@gmail.com>
2024-09-27 14:44:58 -0500

    Proto3 Done

--- 3DPrototype1DuncanBarner/Assets/Scripts/P3/Pro3PlayerController.cs
+++ 3DPrototype1DuncanBarner/Assets/Scripts/P3/Pro3PlayerController.cs
@@ -12,11 +12,28 @@ public class Pro3PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
 
+    
+    public Animator playerAnimator;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip explosionSound;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
+        playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+
+       //starts player in running animation bc speed_f is set to 1.0 and that's the parameter requirement
+        playerAnimator.SetFloat("Speed_f", 1.0f);
+
+      
+
         rb = GetComponent<Rigidbody>();
 
         if (Physics.gravity.y > -10)
@@ -31,7 +48,12 @@ public class Pro3PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             rb.AddForce(Vector3.up * jumpForce, jumpForceMode);
+            playerAudio.PlayOneShot(jumpSound, 0.6f);
+            dirtParticle.Stop();
             isOnGround = false;
+
+            //set jump trigger animation
+            playerAnimator.SetTrigger("Jump_trig");
         }
     }
 
@@ -40,9 +62,15 @@ public class Pro3PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
         else if(collision.gameObject.CompareTag("Obstacle") && !gameOver)
         {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explosionSound, 1.0f);
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+           
             gameOver = true;
         }
       
