bb4716cad6a9ff1607c43f5b3cd63e6e99f2e652
Duncan Barner <duncan.barner@gmail.com>
2024-11-17 14:47:41 -0600

    Challenge 4 halfway done
    (finish increase enemy speed and the additional requirements)

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Duncan Barner
+ * Prototype 4
+ * PlayerController
+ * Player movement
+ */
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    public float speed;
+    private float forwardInput;
+    public bool hasPowerUp;
+    private float powerUpStrength = 15.0f;
+    public GameObject powerupIndicator;
+
+    private GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.FindGameObjectWithTag("FocalPoint");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+
+        //move powerup indicator to ground below our player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Player collided with " + collision.gameObject.name + "with powerup " + hasPowerUp);
+
+            //get local ref to enemy rb
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            //set Vector3 with direction away from player (knock them away)
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position).normalized;
+
+            //add impulse force away from player
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
+    }
+
+   
+}
---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*
+ * Duncan Barner
+ * Prototype 4
+ * SpawnManager
+ * Handles spawning of powerups/enemies and also win condition
+ */
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 8;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerUp;
+    public Text waveCounter;
+    private bool allowSpawn = true;
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(waveNumber, allowSpawn);
+        SpawnPowerup(1, allowSpawn);
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn, bool allowSpawn)
+    {
+        if (allowSpawn == true)
+        {
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                //instantiate enemy in random position
+                Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+            }
+        }
+    }
+
+    private void SpawnPowerup(int powerupsToSpawn, bool allowSpawn)
+    {
+        if (allowSpawn == true)
+        {
+            for (int i = 0; i < powerupsToSpawn; i++)
+            {
+                //instantiate powerup in random position
+                Instantiate(powerUp, GenerateSpawnPosition(), powerUp.transform.rotation);
+            }
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        //generate random position on platform
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if(enemyCount == 0 && allowSpawn == true)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber, allowSpawn);
+            SpawnPowerup(1, allowSpawn);
+        }
+        waveCounter.text = "Wave: " + waveNumber;
+
+
+        if(waveNumber == 10)
+        {
+            allowSpawn = false;
+            if(enemyCount == 0)
+            {
+                waveCounter.text = "You Win! Press R to replay!";
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        
+        if(player.transform.position.y < -10)
+        {
+            allowSpawn = false;
+            enemyCount = 0;
+            waveCounter.text = "You Lose! Press R to retry!";
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+
+}
